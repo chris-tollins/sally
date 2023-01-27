1bfa46ccf657529db6ff06f7c1e2fb68e32667d0
chris-tollins <105399625+chris-tollins@users.noreply.github.com>
2023-01-26 20:27:43 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,10 +8,8 @@ dotenv.config()
 console.log(process.env.OPENAI_API_KEY)
 
 const configuration = new Configuration({
-  apiKey: process.env.OPEN_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
-
-const openai = new OpenAIApi(configuration);
 
 const app = express()
 app.use(cors())
